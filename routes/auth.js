e40f9506a3d37b79600a9aaf02a4fbddf127a803
suraez <oberais50@gmail.com>
2022-01-25 19:42:37 +0545

    google auth implemented

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,11 @@ const passport = require('passport');
 const router = express.Router();
 
 router.get('/logout', (req, res) => {
-    res.send('Logging out....')
+    req.logout();
+    res.json({
+        success: true,
+        msg: 'successfully log out'
+    })
 })
 
 router.get('/google', passport.authenticate('google',{
@@ -12,8 +16,40 @@ router.get('/google', passport.authenticate('google',{
 
 // after user select their account while trying to login
 router.get('/google/redirect',passport.authenticate('google'), (req, res) => {
-    console.log('from the  router');
-    res.send('yay ! you are logged in  !')
+    if (req.user){
+        res.redirect('/auth/profile')
+    } else {
+        res.redirect('/auth/fail')
+    }
 })
 
+// after successful authentication
+router.get('/profile', (req, res) => {
+    res.json({
+        success: true,
+        data: req.user
+    })
+})
+
+// after invalid authentication
+router.get('/fail', (req, res) => {
+    res.json({
+        success: false,
+        msg: 'can not log in the user'
+    })
+})
+
+const authCheck = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/auth/fail')
+    }
+}
+
+
+// defining a protected route
+router.get('/getprofile', authCheck, (req, res) => {
+    res.send('You are able to access this page');
+})
 module.exports = router;
